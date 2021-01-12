352ae7b92434f0b0c4fe60a9defb275e2b196d17
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2021-01-13 04:38:05 +0700

    Update index.js

--- src/elements/Stepper/index.js
+++ src/elements/Stepper/index.js
@@ -1,34 +1,91 @@
-import React, { useState } from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-import propTypes from "prop-types";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  backButton: {
+    marginRight: theme.spacing(1),
+  },
+  instructions: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+}));
 
-export default function Stepper(props) {
-  const { steps, initialStep } = props;
-  const stepsKeys = Object.keys(steps);
-
-  const [CurrentStep, setCurrentStep] = useState(
-    stepsKeys.indexOf(initialStep) > -1 ? initialStep : stepsKeys[0]
-  );
-  const totalStep = stepsKeys.length;
-  const indexStep = stepsKeys.indexOf(CurrentStep);
-
-  function prevStep() {
-    if (+indexStep > 0) setCurrentStep(stepsKeys[indexStep - 1]);
-  }
-
-  function nextStep() {
-    if (+indexStep < totalStep) setCurrentStep(stepsKeys[indexStep + 1]);
-  }
-
-  return <>{props.children(prevStep, nextStep, CurrentStep, steps)}</>;
+function getSteps() {
+  return ['Select master blaster campaign settings', 'Create an ad group', 'Create an ad'];
 }
 
-Stepper.propTypes = {
-  data: propTypes.object.isRequired,
-  initialStep: propTypes.string,
-};
+function getStepContent(stepIndex) {
+  switch (stepIndex) {
+    case 0:
+      return 'Select campaign settings...';
+    case 1:
+      return 'What is an ad group anyways?';
+    case 2:
+      return 'This is the bit I really care about!';
+    default:
+      return 'Unknown stepIndex';
+  }
+}
 
-export { default as Numbering } from "./Numbering";
-export { default as Meta } from "./Meta";
-export { default as Controller } from "./Controller";
-export { default as MainContent } from "./MainContent";
+export default function HorizontalLabelPositionBelowStepper() {
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const steps = getSteps();
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Stepper activeStep={activeStep} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+      <div>
+        {activeStep === steps.length ? (
+          <div>
+            <Typography className={classes.instructions}>All steps completed</Typography>
+            <Button onClick={handleReset}>Reset</Button>
+          </div>
+        ) : (
+          <div>
+            <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+            <div>
+              <Button
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                className={classes.backButton}
+              >
+                Back
+              </Button>
+              <Button variant="contained" color="primary" onClick={handleNext}>
+                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
