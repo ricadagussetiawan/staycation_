4579f0a071c75bed3755c2537ef4b98b9291ef9b
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2021-01-13 04:31:16 +0700

    Update index.js

--- src/elements/Stepper/index.js
+++ src/elements/Stepper/index.js
@@ -7,7 +7,7 @@ export default function Stepper(props) {
   const stepsKeys = Object.keys(steps);
 
   const [CurrentStep, setCurrentStep] = useState(
-    stepsKeys.indexOf(initialStep) > -1 ? initialStep : stepsKeys[1]
+    stepsKeys.indexOf(initialStep) > -1 ? initialStep : stepsKeys[0]
   );
   const totalStep = stepsKeys.length;
   const indexStep = stepsKeys.indexOf(CurrentStep);
