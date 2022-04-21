5c819d48293aa8196bc08aee318b93342bdf782a
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2022-04-21 20:44:09 +0700

    Update BookingInformation.js

--- src/parts/Checkout/BookingInformation.js
+++ src/parts/Checkout/BookingInformation.js
@@ -14,11 +14,7 @@ export default function BookingInformation(props) {
             <Fade delay={300}>
               <div className="card">
                 <figure className="img-wrapper" style={{ height: 270 }}>
-                  <img
-                    className="img-cover"
-                    src={`${process.env.REACT_APP_HOST}/${ItemDetails.imageId[0].imageUrl}`}
-                    alt={ItemDetails.title}
-                  />
+                  <img className="img-cover" src={`${process.env.REACT_APP_HOST}/${ItemDetails.imageId[0].imageUrl}`} alt={ItemDetails.title} />
                 </figure>
                 <div className="row align-items-center">
                   <div className="col">
@@ -31,7 +27,7 @@ export default function BookingInformation(props) {
                   </div>
                   <div className="col-auto">
                     <span>
-                      ${+checkout.duration * ItemDetails.price} USD
+                      Rp{+checkout.duration * ItemDetails.price} 
                       <span className="text-gray-500"> per </span>
                       {checkout.duration} {ItemDetails.unit}
                       {+checkout.duration > 1 ? "s" : ""}
@@ -43,39 +39,17 @@ export default function BookingInformation(props) {
           </div>
           <div className="col-5 py-5" style={{ paddingLeft: 80 }}>
             <Fade delay={600}>
-              <label htmlFor="firstName">First Name</label>
-              <InputText
-                id="firstName"
-                name="firstName"
-                value={data.firstName}
-                onChange={props.onChange}
-              />
+              <label htmlFor="firstName">Nama Depan</label>
+              <InputText id="firstName" name="firstName" value={data.firstName} onChange={props.onChange} />
 
-              <label htmlFor="lastName">Last Name</label>
-              <InputText
-                id="lastName"
-                name="lastName"
-                value={data.lastName}
-                onChange={props.onChange}
-              />
+              <label htmlFor="lastName">Nama Belakang</label>
+              <InputText id="lastName" name="lastName" value={data.lastName} onChange={props.onChange} />
 
               <label htmlFor="email">Email Address</label>
-              <InputText
-                id="email"
-                name="email"
-                type="email"
-                value={data.email}
-                onChange={props.onChange}
-              />
+              <InputText id="email" name="email" type="email" value={data.email} onChange={props.onChange} />
 
               <label htmlFor="phone">Phone Number</label>
-              <InputText
-                id="phone"
-                name="phone"
-                type="tel"
-                value={data.phone}
-                onChange={props.onChange}
-              />
+              <InputText id="phone" name="phone" type="tel" value={data.phone} onChange={props.onChange} />
             </Fade>
           </div>
         </div>
