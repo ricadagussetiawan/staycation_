cea373dff7013b1f01d87bb1e01c52b98043662c
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2022-04-21 19:16:49 +0700

    Update Payment.js

--- src/parts/Checkout/Payment.js
+++ src/parts/Checkout/Payment.js
@@ -9,9 +9,9 @@ import logoMandiri from "assets/images/logo-mandiri.jpg";
 export default function Payment(props) {
   const { data, ItemDetails, checkout } = props;
 
-  const tax = 10;
+  const ppn = 10;
   const subTotal = ItemDetails.price * checkout.duration;
-  const grandTotal = (subTotal * tax) / 100 + subTotal;
+  const grandTotal = (subTotal * ppn) / 100 + subTotal;
 
   return (
     <Fade>
@@ -20,9 +20,9 @@ export default function Payment(props) {
           <div className="col-5 border-right py-5" style={{ paddingRight: 80 }}>
             <Fade delay={300}>
               <p className="mb-4">Transfer Pembayaran:</p>
-              <p>Tax: {tax}%</p>
-              <p>Sub total: ${subTotal} USD</p>
-              <p>Total: ${grandTotal} USD</p>
+              <p>ppn: {ppn}%</p>
+              <p>Sub total: Rp{subTotal} </p>
+              <p>Total: Rp{grandTotal} </p>
               <div className="row mt-4">
                 <div className="col-3 text-right">
                   <img src={logoBca} alt="bank central asia" width="60" />
@@ -53,31 +53,13 @@ export default function Payment(props) {
           <div className="col-5 py-5" style={{ paddingLeft: 80 }}>
             <Fade delay={600}>
               <label htmlFor="proofPayment">Upload Bukti Transfer</label>
-              <InputFile
-                accept="image/*"
-                id="proofPayment"
-                name="proofPayment"
-                value={data.proofPayment}
-                onChange={props.onChange}
-              />
+              <InputFile accept="image/*" id="proofPayment" name="proofPayment" value={data.proofPayment} onChange={props.onChange} />
 
               <label htmlFor="bankName">Asal Bank</label>
-              <InputText
-                id="bankName"
-                name="bankName"
-                type="text"
-                value={data.bankName}
-                onChange={props.onChange}
-              />
+              <InputText id="bankName" name="bankName" type="text" value={data.bankName} onChange={props.onChange} />
 
               <label htmlFor="bankHolder">Nama Pengirim</label>
-              <InputText
-                id="bankHolder"
-                name="bankHolder"
-                type="text"
-                value={data.bankHolder}
-                onChange={props.onChange}
-              />
+              <InputText id="bankHolder" name="bankHolder" type="text" value={data.bankHolder} onChange={props.onChange} />
             </Fade>
           </div>
         </div>
