37b24c2e7a827cf5bd92cd6abffe5f88d0a42094
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2022-08-16 16:28:25 +0700

    Update Footer.js

--- src/parts/Footer.js
+++ src/parts/Footer.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import Button from "elements/Button";
 import IconText from "parts/IconText";
+// import TermConditions from "assets/images/Capture.jpg";
+
 export default function Footer() {
   return (
     <footer>
@@ -9,77 +11,79 @@ export default function Footer() {
         <div className="row">
           <div className="col-auto" style={{ width: 350 }}>
             <IconText />
-            <p className="brand-tagline">
-              We kaboom your beauty holiday instantly and memorable.
-            </p>
+            <p className="brand-tagline">Selamat datang di Staycation .</p>
           </div>
           <div className="col-auto mr-5">
-            <h6 className="mt-2">For Beinners</h6>
+            <h6 className="mt-2">Untuk Pemula</h6>
             <ul className="list-group list-group-flush">
               <li className="list-group-item">
                 <Button type="link" href="/register">
-                  New Account
+                  Buat Akun
                 </Button>
               </li>
-              <li className="list-group-item">
+              {/* <li className="list-group-item">
                 <Button type="link" href="/properties">
-                  Start Booking a Room
+                  Mulai Pesan Kamar
                 </Button>
-              </li>
+              </li> */}
               <li className="list-group-item">
-                <Button type="link" href="/use-payments">
-                  Use Payments
-                </Button>
+                <a href="https://www.sketchappsources.com/resources/source-image/indonesian-payment-methods-icons-anas-kurniawan.png" class="list-group active">
+                  Gunakan Pembayaran
+                </a>
               </li>
             </ul>
           </div>
           <div className="col-2 mr-5">
-            <h6 className="mt-2">Explore Us</h6>
+            <h6 className="mt-2">Jelajahi</h6>
             <ul className="list-group list-group-flush">
               <li className="list-group-item">
-                <Button type="link" href="/careers">
-                  Our Careers
-                </Button>
+              <a href="https://www.cekindo.com/careers" class="list-group active">
+                  Karir
+                </a>
               </li>
               <li className="list-group-item">
-                <Button type="link" href="/privacy">
-                  Privacy
-                </Button>
+              <a href="https://www.linkedin.com/in/ricad-agus-setiawan-53183a167/" class="list-group active">
+                  Pribadi
+                </a>
               </li>
-              <li className="list-group-item">
-                <Button type="link" href="/terms">
-                  Terms & Conditions
-                </Button>
-              </li>
+              {/* <li className="list-group-item">
+              <a href="https://www.linkedin.com/in/ricad-agus-setiawan-53183a167/" class="list-group active">
+              Syarat & Ketentuan
+                </a>
+              </li>  */}
             </ul>
           </div>
           <div className="col-3">
-            <h6 className="mt-2">Connect Us</h6>
+            <h6 className="mt-2">Hubungi Kami</h6>
             <ul className="list-group list-group-flush">
               <li className="list-group-item">
-                <Button
-                  isExternal
-                  type="link"
-                  href="mailto:support@staycation.id"
-                >
+                <Button isExternal type="link" href="mailto:ichacnakval@gmail.com">
                   ichacnakval@gmail.com
                 </Button>
               </li>
               <li className="list-group-item">
-                <Button isExternal type="link" href="tel:+622122081996">
+                <Button isExternal type="link" href="tel:+02142882913">
                   021 - 4288 - 2913
                 </Button>
               </li>
               <li className="list-group-item">
+                <Button isExternal type="link" href="tel:+6281292931896">
+                0812 - 9293 -1896
+                </Button>
+              </li>
+              {/* <li className="list-group-item">
                 <span>Staycation, Kemang, Jakarta</span>
+              </li> */}
+              <li className="list-group-item">
+                <Button isExternal type="link" href="https://www.google.com/maps/uv?pb=!1s0x2e69f1f0430433cf%3A0x6b92d13c2900de0e!3m1!7e115!4shttps%3A%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipMXPfowQOfq0R58tAU8hnVjfIfRisFZAAN97DWn%3Dw373-h180-k-no!5sstaycation%20in%20kemang%20vilage%20-%20Panyliksikan%20Google!15sCgIgAQ&imagekey=!1e10!2sAF1QipMXPfowQOfq0R58tAU8hnVjfIfRisFZAAN97DWn&hl=ban&sa=X&ved=2ahUKEwilqYqeiMn5AhVgH7cAHe9XCycQoip6BAhDEAM">
+                <span>Staycation, Kemang, Jakarta</span>
+                </Button>
               </li>
             </ul>
           </div>
         </div>
         <div className="row">
-          <div className="col text-center copyrights">
-            Copyright 2019 • Ricad Agustin • Staycation
-          </div>
+          <div className="col text-center copyrights">Copyright 2019 • Ricad Agus Setiawan • Staycation</div>
         </div>
       </div>
     </footer>
