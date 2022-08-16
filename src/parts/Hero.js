280e567086e308d33746883798e7acb5ca4171c2
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2022-08-16 18:22:08 +0700

    Update Hero.js

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -22,15 +22,15 @@ export default function Hero(props) {
         <div className="row align-items-center">
           <div className="col-auto pr-5" style={{ width: 530 }}>
             <h1 className="font-weight-bold line-height-1 mb-3">
-              Forget Busy Work, <br />
-              Start Next Vacation
+            Lupakan Pekerjaan Sibuk, <br />
+              Mulai Liburan 
             </h1>
             <p
               className="mb-4 font-weight-light text-gray-500 w-75"
               style={{ lineHeight: "170%" }}
             >
-              We provide what you need to enjoy your holiday with family. Time
-              to make another memorable moments.
+              Kami menyediakan apa yang Anda butuhkan untuk menikmati liburan Anda bersama keluarga. Waktu
+              untuk membuat momen tak terlupakan lainnya.
             </p>
             <Button
               className="btn px-5"
@@ -38,7 +38,7 @@ export default function Hero(props) {
               isPrimary
               onClick={showMostPicked}
             >
-              Show Me Now
+              Perlihatkan padaku sekarang
             </Button>
 
             <div className="row" style={{ marginTop: 80 }}>
@@ -52,7 +52,7 @@ export default function Hero(props) {
                 <h6 className="mt-3">
                   {formatNumber(props.data.travelers)}{" "}
                   <span className="text-gray-500 font-weight-light">
-                    travelers
+                    explorasi
                   </span>
                 </h6>
               </div>
@@ -66,7 +66,7 @@ export default function Hero(props) {
                 <h6 className="mt-3">
                   {formatNumber(props.data.treasures)}{" "}
                   <span className="text-gray-500 font-weight-light">
-                    treasures
+                    Photo
                   </span>
                 </h6>
               </div>
@@ -80,7 +80,7 @@ export default function Hero(props) {
                 <h6 className="mt-3">
                   {formatNumber(props.data.cities)}{" "}
                   <span className="text-gray-500 font-weight-light">
-                    cities
+                    Kota
                   </span>
                 </h6>
               </div>
